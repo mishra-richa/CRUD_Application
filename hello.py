1cc6e4b9d384741c48dacd08254e2ef2637d0a38
Richa Mishra <mricha.0228@gmail.com>
2024-12-08 13:32:12 +0530

    added styles.css file and modified hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
 
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    pass
+    return render_template('index.html')
 
 if __name__ in "__main__":
     app.run(debug=True)
